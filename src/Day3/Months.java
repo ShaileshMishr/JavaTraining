0a7435ebbf8e33f5e29205f576c798a68dfa9141
shaileshm <shaileshmishra9907@gmail.com>
2022-11-26 02:22:14 +0530

    Completed

--- src/Day3/Months.java
+++ src/Day3/Months.java
@@ -6,70 +6,43 @@
 //   You should use Enum in this program.
 
 package JavaBasics.src.Day3;
-import java.time.Month;
+
 import java.util.Scanner;
-enum day{
-    January,
-    February,
-    March,
-    April,
-    May,
-    June,
-    July,
-    August,
-    September,
-    October,
-    November,
-    December
-}
-public class Months {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Number from 1 to 12: ");
-        int choice = sc.nextInt();
-        switch (choice){
-            case 1:
-                System.out.println(Month.JANUARY);
-                break;
 
-            case 2:
-                System.out.println(Month.FEBRUARY);
-                break;
-            case 3:
-                System.out.println(Month.MARCH);
-                break;
-            case 4:
-                System.out.println(Month.APRIL);
-                break;
-            case 5:
-                System.out.println(Month.MAY);
-                break;
-            case 6:
-                System.out.println(Month.JUNE);
-                break;
-            case 7:
-                System.out.println(Month.JULY);
-                break;
-            case 8:
-                System.out.println(Month.AUGUST);
-                break;
-            case 9:
-                System.out.println(Month.SEPTEMBER);
-                break;
-            case 10:
-                System.out.println(Month.OCTOBER);
-                break;
-            case 11:
-                System.out.println(Month.NOVEMBER);
-                break;
-            case 12:
-                System.out.println(Month.DECEMBER);
-                break;
-            default:
-                System.out.println("You have entered wrong Input");
+public enum Months {
 
-
-        }
+    January(1),
+    Febraury(2),
+    March(3),
+    April(4),
+    May(5),
+    june(6),
+    july(7),
+    august(8),
+    september(9),
+    october(10),
+    november(11),
+    december(12);
+    private int month;
+    private Months(int month)
+    {
+        this.month=month;
     }
-}
 
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter month : ");
+        Scanner sc = new Scanner(System.in);
+        int num=sc.nextInt();
+        for(Months monthName:Months.values())
+        {
+            if( monthName.month==num)
+            {
+                System.out.println(monthName);
+            }
+        }
+
+    }
